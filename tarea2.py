54b6a29e85485b5241a72cb652eb5d51e3c52e10
2231174797 <31254610+2231174797@users.noreply.github.com>
2017-09-01 13:22:23 -0500

    Add files via upload
    Mascotas por edad

--- tarea2.py
+++ tarea2.py
@@ -1,5 +1,11 @@
 import matplotlib.pyplot as plt
-x = [1,2,3,4,5]
-y = [1,4,9,16,25]
+x = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21]
+y = [0,0,0,0,0,0,0,1,1,2,2,3,3,4,4,4,4,4,5,5,5,5]
 plt.plot(x, y)
+plt.ylabel('mascotas por edad[a-nos]')
 plt.savefig('temp2.png')
+
+
+
+
+
